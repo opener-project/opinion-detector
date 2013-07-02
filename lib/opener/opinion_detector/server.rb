64101f3db8fe8a8f1349585390e8e79e75b25d49
Yorick Peterse <yorickpeterse@olery.com>
2013-07-02 18:01:03 +0200

    Catch argument errors in the webservice.
    Signed-off-by: Yorick Peterse <yorickpeterse@olery.com>

--- lib/opener/opinion_detector/server.rb
+++ lib/opener/opinion_detector/server.rb
@@ -11,6 +11,19 @@ module Opener
       set :views, File.expand_path('../views', __FILE__)
       text_processor OpinionDetector
       accepted_params :input
+
+      ##
+      # @see Opener::Webservice#analyze
+      #
+      def analyze(*args)
+        begin
+          super
+        # ArgumentErrors are used for invalid languages. These happen too often
+        # so we'll supress them for now.
+        rescue ArgumentError => error
+          halt(400, error.message.strip)
+        end
+      end
     end # Server
   end # OpinionDetector
 end # Opener
